3ed90a7a262443b380414a142391e506bf1a92f7
akun <victorsos@163.com>
2013-09-28 01:06:11 +0800

    demo: new ticket page, change HTML tempalte to AngularJS template

--- atrac/static/front_end/app/scripts/controllers/new_ticket.js
+++ atrac/static/front_end/app/scripts/controllers/new_ticket.js
@@ -2,9 +2,42 @@
 
 angular.module('frontEndApp')
   .controller('NewTicketCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.types = [
+      '缺陷',
+      '改进/建议',
+      '功能',
+      '支持'
+    ];
+    $scope.milestones = [
+      '1.0.0',
+      '1.0.1',
+      '1.0.2',
+      '1.1.0',
+      '2.0.0',
+      '3.0.0',
+      '4.0.0'
+    ];
+    $scope.versions = [
+      '1.0.0',
+      '1.0.1',
+      '1.0.2',
+      '1.1.0'
+    ];
+    $scope.categorys = [
+      '模块1',
+      '模块2',
+      '模块3',
+      '模块4'
+    ];
+    $scope.assigneds = [
+      '<<我>>',
+      'user1',
+      'user2',
+      'user3'
+    ];
+    $scope.ccs = [
+      'user1',
+      'user2',
+      'user3'
     ];
   });
