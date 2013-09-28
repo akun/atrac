7ecbee8be87a900cb6a02ea117ada6d898c2cc64
akun <victorsos@163.com>
2013-09-28 23:53:11 +0800

    demo: ticket list page

--- atrac/static/front_end/app/scripts/controllers/main.js
+++ atrac/static/front_end/app/scripts/controllers/main.js
@@ -7,9 +7,93 @@ angular.module('frontEndApp')
     };
   })
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.tickets = [{
+      'id': 10,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 9,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 8,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 7,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 6,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 5,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 4,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 3,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 3,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 2,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }, {
+      'id': 1,
+      'type': '缺陷',
+      'status': '关闭',
+      'priority': '高',
+      'summary': '添加不能成功',
+      'assigned': 'user1',
+      'milestone': '2.0.0',
+    }];
   });
