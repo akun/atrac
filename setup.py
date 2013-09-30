e0bad895b9424784ac9737de58a45d03bc68038f
akun <victorsos@163.com>
2013-09-30 21:18:13 +0800

    add setup.py, and change git ignore rule

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='atrac',
+    version='0.0.1',
+    description='A SCMS',
+    author='akun',
+    url='https://github.com/akun/atrac',
+    packages=[
+        'atrac',
+    ],
+    package_dir={'atrac': 'atrac'},
+    install_requires=[
+        'tornado==3.1.1',
+        'mongokit==0.9.0',
+    ],
+)
