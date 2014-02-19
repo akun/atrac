62047bc4981aa887b89a6a0cb578d2b186329346
akun <victorsos@163.com>
2014-02-20 02:00:14 +0800

    demo: souce list page, tree table effect

--- atrac/static/front_end/app/scripts/controllers/source.js
+++ atrac/static/front_end/app/scripts/controllers/source.js
@@ -2,12 +2,23 @@
 
 angular.module('frontEndApp')
   .controller('SourceCtrl', function ($scope, $resource) {
+    var formatNode = function (node) {
+      if (node.kind === 'file') {
+        node.url = '#/source/path/' + node.path;
+        node.icon = 'icon-file';
+      } else if (node.kind === 'folder') {
+        node.url = '#/source';
+        node.icon = 'icon-folder-close';
+      }
+      node.isVisible = false;
+      node.subNodes = [];
+    };
+
     var Source = $resource('/a/source/read');
     Source.get({}, function (data) {
       $scope.nodes = data.result.nodes;
       angular.forEach($scope.nodes, function (node) {
-        node.subNodes = [];
-        node.isVisible = false;
+        formatNode(node);
       });
     });
 
@@ -17,13 +28,7 @@ angular.module('frontEndApp')
       Source.get({path: node.path}, function (data) {
         node.subNodes = data.result.nodes;
         angular.forEach(node.subNodes, function (node) {
-          if (node.kind === 'file') {
-            node.icon = 'icon-file';
-          } else if (node.kind === 'folder') {
-            node.icon = 'icon-folder-close';
-          }
-          node.subNodes = [];
-          node.isVisible = false;
+          formatNode(node);
         });
       });
     };
